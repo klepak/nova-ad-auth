0796219cce6f51a9d921131155f19e6b180a47ca
Knut Leborg <knut.leborg@telecomputing.no>
2019-03-30 18:09:58 +0100

    fix namespacing

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Klepak\LaravelAuth\Models;
+namespace Klepak\NovaAdAuth\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
